931f064849d1c295f2f9cda2ec783e7dd75f5f9e
LoGaN <80800012+LGNLogAN@users.noreply.github.com>
2023-09-08 10:41:45 +0900

    Create Part_1.java

---
+++ DataType/Part_1.java
@@ -0,0 +1,10 @@
+package Part_1;
+
+public class DataType {
+	public static void main(String[] args) {
+		int int_1 = 50;    // int_1 변수에 숫자형 50을 선언 | 변수 선언과 동시에 초기화
+		char char_1 = 'A'; // char_1 변수에 문자형 A를 선언 | 변수 선언과 동시에 초기화
+		
+		
+	}
+}
