f928da3183b427e37efc2331eaab5d9ad44f37f0
LoGaN <80800012+LGNLogAN@users.noreply.github.com>
2023-09-08 12:51:26 +0900

    Create ChangeType.java

---
+++ DataType/ChangeType.java
@@ -0,0 +1,13 @@
+package DataType;
+
+public class ChangeType {
+
+	public static void main(String[] args) {
+		int n = 10;
+		double dnum = n;
+		System.out.println(dnum);
+		
+		double dnum2 = 20;
+		int n = dnum2;
+	}
+}
